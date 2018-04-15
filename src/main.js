0dc4d6059a0531ca77f172cd35b7e967e09b8d4e
Vincent Gercke Pedersen <vgercke@gmail.com>
2018-04-15 15:47:03 +0200

    Vuex store setup 📃

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
+// Eksterne imports
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
+// Interne imports
+import App from './App.vue';
+import router from './router';
+import store from './store/store';
+import { apiBaseUrl } from './config/config';
+
+// Vi fortæller Vue at vi bruger Axios
 Vue.use(VueAxios, axios);
 
+// Sæt base URL til når vi laver API requests
+axios.defaults.baseURL = apiBaseUrl;
+
+// Denne bestemmer om vi får vist hjælp i browser loggen
 Vue.config.productionTip = true;
 
 new Vue({
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+
+  },
+  getters: {
 
   },
   mutations: {
@@ -13,4 +16,4 @@ export default new Vuex.Store({
   actions: {
 
   }
-})
+});
